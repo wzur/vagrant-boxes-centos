5cb98f2c214569c449670d0e227204694c1d849e
Waldemar Zurowski <wzurowski@rhythmone.com>
2015-10-21 13:28:37 +0100

    Added possibility to select virtualisation to build for

--- run-packer.sh
+++ run-packer.sh
@@ -1,9 +1,22 @@
 #!/bin/bash -eu
+
+VIRT_ENV="virtualbox-iso qemu"
+if [ "x${1}" = "x-qemu" ]
+then
+  shift
+  VIRT_ENV=qemu
+fi
+if [ "x${1}" = "x-virtualbox" ]
+then
+  shift
+  VIRT_ENV=virtualbox-iso
+fi
+
 for template
 do
   version=${template#template-}
   version=${version%.json}
-  for virt in virtualbox-iso qemu
+  for virt in ${VIRT_ENV}
   do
     ATLAS_USERNAME=wzurowski ATLAS_NAME="${version}" PATH=~/packer:$PATH packer build  -only="${virt}" "${template}"
   done
